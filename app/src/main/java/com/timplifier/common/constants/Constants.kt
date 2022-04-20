d585499c1edf3c767261ddcf5ff438d22b91fcad
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-20 22:38:38 +0600

    clean architecture + constants

---
+++ app/src/main/java/com/timplifier/common/constants/Constants.kt
@@ -0,0 +1,6 @@
+package com.timplifier.common.constants
+
+object Constants {
+    const val BASE_URL = "https://kitsu.io/api/edge/"
+    const val ANIME_GET_REQUEST = "anime"
+}
---
+++ app/src/main/java/com/timplifier/kitsu/data/remote/apiservices/AnimeApiService.kt
@@ -0,0 +1,9 @@
+package com.timplifier.kitsu.data.remote.apiservices
+
+import com.timplifier.common.constants.Constants.ANIME_GET_REQUEST
+import retrofit2.http.GET
+
+interface AnimeApiService {
+    @GET(ANIME_GET_REQUEST)
+    suspend fun fetchAnime()
+}
