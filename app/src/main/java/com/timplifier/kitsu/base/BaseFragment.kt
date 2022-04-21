197a81970132416526ad31df25ace61e48a9f8b7
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-21 15:49:31 +0600

    add safeflowgather function to collect flow with safety

--- app/src/main/java/com/timplifier/kitsu/base/BaseFragment.kt
+++ app/src/main/java/com/timplifier/kitsu/base/BaseFragment.kt
@@ -5,10 +5,13 @@ import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.viewbinding.ViewBinding
 import com.timplifier.kitsu.presentation.ui.state.UiState
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 abstract class BaseFragment<Binding : ViewBinding, ViewModel : BaseViewModel>(@LayoutRes layoutId: Int) :
     Fragment(
@@ -44,7 +47,7 @@ abstract class BaseFragment<Binding : ViewBinding, ViewModel : BaseViewModel>(@L
 
     }
 
-    protected fun <T> StateFlow<UiState<T>>.spectateUiState(
+    protected suspend fun <T> StateFlow<UiState<T>>.spectateUiState(
         lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
         success: ((data: T) -> Unit)? = null,
         loading: ((data: UiState.Loading<T>) -> Unit)? = null,
@@ -52,15 +55,29 @@ abstract class BaseFragment<Binding : ViewBinding, ViewModel : BaseViewModel>(@L
         idle: ((idle: UiState.Idle<T>) -> Unit)? = null,
         gatherIfSucceed: ((state: UiState<T>) -> Unit)? = null
     ) {
-        collect {
-            gatherIfSucceed?.invoke(it)
-            when (it) {
-                is UiState.Idle -> idle?.invoke(it)
-                is UiState.Loading -> loading?.invoke(it)
-                is UiState.Error -> error?.invoke(it.error)
-                is UiState.Success -> success?.invoke(it.data)
+        safeFlowGather(lifecycleState) {
+            collect {
+                gatherIfSucceed?.invoke(it)
+                when (it) {
+                    is UiState.Idle -> idle?.invoke(it)
+                    is UiState.Loading -> loading?.invoke(it)
+                    is UiState.Error -> error?.invoke(it.error)
+                    is UiState.Success -> success?.invoke(it.data)
+                }
             }
+
         }
 
+    }
+
+    private fun safeFlowGather(
+        lifecycleState: Lifecycle.State,
+        gather: suspend () -> Unit
+    ) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(lifecycleState) {
+                gather()
+            }
+        }
     }
 }
