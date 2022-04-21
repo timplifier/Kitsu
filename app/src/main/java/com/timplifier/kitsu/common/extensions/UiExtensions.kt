79b6f1af2e0559658bed465f26249ddff1dde2fc
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-21 18:08:47 +0600

    almost finished adapter

--- app/src/main/java/com/timplifier/kitsu/common/extensions/UiExtensions.kt
+++ app/src/main/java/com/timplifier/kitsu/common/extensions/UiExtensions.kt
@@ -4,3 +4,10 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 fun ImageView.loadImageWithGlide(url: String) = Glide.with(this).load(url).into(this)
+fun ImageView.setAnimeStatus(status: String) {
+    when (status) {
+        "finished" -> this.setImageResource()
+
+    }
+
+}
--- app/src/main/java/com/timplifier/kitsu/presentation/ui/adapters/anime/AnimeAdapter.kt
+++ app/src/main/java/com/timplifier/kitsu/presentation/ui/adapters/anime/AnimeAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.timplifier.kitsu.base.BaseDiffUtil
+import com.timplifier.kitsu.common.extensions.loadImageWithGlide
 import com.timplifier.kitsu.data.remote.dtos.anime.Data
 import com.timplifier.kitsu.databinding.ItemAnimeBinding
 
@@ -31,8 +32,18 @@ class AnimeAdapter(
     inner class AnimeViewHolder(private val binding: ItemAnimeBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: Data) {
+            val amountOfEpisodes = data.animeDto.episodeCount
             binding.apply {
                 root.setOnClickListener {
+
+                    tvAverageRating.text = data.animeDto.averageRating
+                    imAnime.loadImageWithGlide(data.animeDto.posterImage.original)
+                    tvEnglishTitle.text = data.animeDto.titles.en
+                    tvJapaneseTitle.text = data.animeDto.titles.jaJp
+                    tvAmountOfEpisodes.text = "Episodes : $amountOfEpisodes TV"
+                    imStatus.setImageResource()
+                    tvStartDate.text = data.animeDto.startDate
+                    tvEndDate.text = data.animeDto.endDate
 
                     onItemClick(data.id)
                 }
