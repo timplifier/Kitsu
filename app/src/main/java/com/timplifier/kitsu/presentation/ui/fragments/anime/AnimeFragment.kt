89d2802ad45c378f79e9582f90b0cd6d1a83e2b7
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-23 16:47:09 +0600

    manga

--- app/src/main/java/com/timplifier/kitsu/presentation/ui/fragments/anime/AnimeFragment.kt
+++ app/src/main/java/com/timplifier/kitsu/presentation/ui/fragments/anime/AnimeFragment.kt
@@ -2,12 +2,10 @@ package com.timplifier.kitsu.presentation.ui.fragments.anime
 
 import android.util.Log
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timplifier.kitsu.R
 import com.timplifier.kitsu.base.BaseFragment
-import com.timplifier.kitsu.common.extensions.directionsSafeNavigation
 import com.timplifier.kitsu.common.extensions.isInternetAvailable
 import com.timplifier.kitsu.common.extensions.submitData
 import com.timplifier.kitsu.databinding.FragmentAnimeBinding
@@ -49,11 +47,7 @@ class AnimeFragment : BaseFragment<FragmentAnimeBinding, AnimeViewModel>(R.layou
     }
 
     private fun onItemClick(id: String) {
-        findNavController().directionsSafeNavigation(
-            AnimeFragmentDirections.actionAnimeFragmentToDetailedAnimeFragment(
-                id
-            )
-        )
+
     }
 }
 
