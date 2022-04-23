e37607d9f5685d66594529ebdb87a47814ef5a34
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-23 16:43:43 +0600

    add get constants to constants in common package

--- app/src/main/java/com/timplifier/kitsu/common/constants/Constants.kt
+++ app/src/main/java/com/timplifier/kitsu/common/constants/Constants.kt
@@ -2,6 +2,10 @@ package com.timplifier.kitsu.common.constants
 
 object Constants {
     const val BASE_URL = "https://kitsu.io/api/edge/"
+
     const val GET_ANIME = "anime"
     const val GET_ANIME_BY_ID = "anime/{id}"
+
+    const val GET_MANGA = "manga"
+    const val GET_MANGA_BY_ID = "manga/{id}"
 }
