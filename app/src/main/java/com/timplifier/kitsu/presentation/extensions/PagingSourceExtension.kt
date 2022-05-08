02aca91cf6e0df01ef45e98678374d372e929f87
Clar1fy <iamonmydoctorpath@gmail.com>
2022-05-08 14:14:19 +0600

    toying with retrofit

--- app/src/main/java/com/timplifier/kitsu/presentation/extensions/PagingSourceExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.timplifier.kitsu.presentation.extensions
-
-import androidx.paging.PagingSource
-
-
-fun <T : Any> PagingSource.LoadResult.Page.Companion.loadResult(data: T, prevKey: Any, nextKey: Any) {
-    PagingSource.LoadResult.Page.Companion(data,prevKey,nextKey)
-}
--- data/src/main/kotlin/com/timplifier/kitsu/data/remote/dtos/base/BaseResponseDto.kt
+++
@@ -1,8 +0,0 @@
-package com.timplifier.kitsu.data.remote.dtos.base
-
-data class BaseResponseDto<T>(
-    val first: String?,
-    val prev: String?,
-    val next: String?,
-    val last: String?
-)
