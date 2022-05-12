f41e85495559216539c52d3c84e125da280490fe
Clar1fy <iamonmydoctorpath@gmail.com>
2022-05-12 19:45:40 +0600

    sign in fragment design

---
+++ app/src/main/java/com/timplifier/kitsu/presentation/ui/fragments/main/MainFlowFragment.kt
@@ -0,0 +1,10 @@
+package com.timplifier.kitsu.presentation.ui.fragments.main
+
+import com.timplifier.kitsu.R
+import com.timplifier.kitsu.presentation.base.BaseFlowFragment
+
+
+class MainFlowFragment :
+    BaseFlowFragment(R.layout.fragment_main_flow, R.id.nav_host_main_fragment) {
+
+}
